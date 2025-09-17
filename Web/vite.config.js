ebe168db3f8fd68788205030afd3c7c404411354
Spyros <spyros.catechis@bwdbern.ch>
2025-09-17 15:00:22 +0200

    updated allowed hosts for web

--- Web/vite.config.js
+++ Web/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'helveticraft.com',
+      'localhost', // Keep localhost for local testing
+      // Add any other hosts you want to allow
+    ],
+  },
 })
