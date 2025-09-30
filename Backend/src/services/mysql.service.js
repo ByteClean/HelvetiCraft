2588a6fc9f689df03ff795a4fd262ededd494408
Nicolas Ammeter <nicolas.ammeter@hotmail.com>
2025-09-30 17:14:55 +0200

    changed incorrect routing

--- Backend/src/services/mysql.service.js
+++ Backend/src/services/mysql.service.js
@@ -1,14 +1,15 @@
+// src/services/mysql.service.js
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT || 3306),
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "",
-  database: process.env.DB_NAME || "authme",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  charset: "utf8mb4"
+  charset: process.env.DB_CHARSET || "utf8mb4"
 });
 
 export default pool;
