b5d907b8dabbccc49052726f4d82542a29365423
Nicolas Ammeter <nicolas.ammeter@hotmail.com>
2026-01-09 21:18:41 +0100

    trying to fix issues

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,12 +3,12 @@ dotenv.config();
 import app from "./app.js";
 import { startPhaseScheduler } from "./jobs/phases.scheduler.js";
 
-startPhaseScheduler({
-  intervalMs:  1000, // 12 Stunden
-  daysForMinVotes: 10,
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`API laeuft auf http://localhost:${PORT}`);
+});
+startPhaseScheduler({
+  intervalMs:  1000, // 12 Stunden
+  daysForMinVotes: 10,
 });
