4e63fd17867698eaa006629b07c72f0f2e4dfaa2
Spyros <spyros.catechis@bwdbern.ch>
2026-01-14 08:49:09 +0100

    removed use unifiedTopology

--- Backend/src/services/mongodb.service.js
+++ Backend/src/services/mongodb.service.js
@@ -11,7 +11,7 @@ let db;
 
 export async function connectMongo() {
   if (!client) {
-    client = new MongoClient(uri, { useUnifiedTopology: true });
+    client = new MongoClient(uri);
     await client.connect();
     console.log('Connected to MongoDB');
     db = client.db(dbName);
