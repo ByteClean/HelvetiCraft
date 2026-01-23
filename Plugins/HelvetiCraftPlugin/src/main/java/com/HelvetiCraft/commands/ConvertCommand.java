661f1d97d1d167b0c219c749f16f085c3c9dc0a7
PetabyteBrain <153134032+PetabyteBrain@users.noreply.github.com>
2026-01-23 09:49:46 +0100

    Added automatic updating of taxconfigs

--- Plugins/HelvetiCraftPlugin/src/main/java/com/HelvetiCraft/commands/ConvertCommand.java
+++ Plugins/HelvetiCraftPlugin/src/main/java/com/HelvetiCraft/commands/ConvertCommand.java
@@ -6,6 +6,8 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import static com.HelvetiCraft.requests.TaxRequests.loadAllTaxConfigFromBackend;
+
 public class ConvertCommand implements CommandExecutor {
 
     private final ConvertManager convertManager;
@@ -25,6 +27,7 @@ public class ConvertCommand implements CommandExecutor {
             return true;
         }
 
+        loadAllTaxConfigFromBackend();
         convertManager.openConvertMenu(p);
         return true;
     }
